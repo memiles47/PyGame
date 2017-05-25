3c20fc1a493b1f0121224e0e8277d9439fb5587a
Michael E Miles <michael@miles-me.net>
2017-05-25 15:04:40 -0500

    Initial Commit

--- PyGame/PyGame.py
+++ PyGame/PyGame.py
@@ -1,1 +1,26 @@
+board = [" " for i in range(9)]
+
+def PrintBoard():
+    row1 = "| {} | {} | {} |".format(board[0], board[1], board[2])
+    row2 = "| {} | {} | {} |".format(board[3], board[4], board[5])
+    row3 = "| {} | {} | {} |".format(board[6], board[7], board[8])
+
+    print()
+    print(row1)
+    print(row2)
+    print(row3)
+    print()
+
+PrintBoard()
+
+while True:
+    choice = int(input("Enter your move (1-9): "))
+
+    if board[choice - 1] == " ":
+        board[choice -1] = "X"
+    else:
+        print()
+        print("That space is taken!")
+
+    PrintBoard()
 
